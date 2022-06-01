3afb57a804511df5977f99421023506b9a06b070
zaghloulegy <zaghloulegy@gmail.com>
2022-06-01 12:10:34 +0100

    madlib is done

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("verb1:")
+verb2 = input("verb2:")
+famous_person = input("famous:")
+
+madlib = f"computer programming is so {adj}! It makes me  so excited all the time because \ I love to {verb1}. Stay happy and {verb2} like you are {famous_person}!"
+
+
+print(madlib)
