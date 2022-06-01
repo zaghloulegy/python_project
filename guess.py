f4822c7ce87ffe24facf880aa3583501b5d30c87
zaghloulegy <zaghloulegy@gmail.com>
2022-06-01 12:34:48 +0100

    computer is guessing

--- guess.py
+++ guess.py
@@ -11,4 +11,21 @@ def guess(x):
             print('sorry, too high')
 
             print(f'yay {random_number}')
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        guess = random.randint(low, high)
+        feedback = input(f'is {guess} too high (H), too low (L), correct (c)').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+print(f"yay {guess}")
+    
+computer_guess(10)
